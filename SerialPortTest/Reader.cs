980e902ae28d83f7c83dcb1b4a36b652e89885f8
dabukofyuslesnoleyj <chinomanga@outlook.com>
2020-05-28 10:21:04 +0800

    added Listeners to serial prot reader and writer

--- SerialPortTest/Reader.cs
+++ SerialPortTest/Reader.cs
@@ -1,15 +1,25 @@
+using System.Collections.Generic;
 using System.IO.Ports;
+using System.Xml.Linq;
 
 namespace SerialPortTest
 {
     interface IReader
     {
         string read();
+        void Attach(IReaderListener listener);
+        void NotifyAll(string s);
+    }
+
+    interface IReaderListener
+    {
+        void update(string s);
     }
 
     class SerialPortReader : IReader
     {
         SerialPort serialPort;
+        List<IReaderListener> readerListeners;
 
         public SerialPortReader()
         {
@@ -19,6 +29,17 @@ namespace SerialPortTest
         public string read()
         {
             return "";
+        }
+
+        public void Attach(IReaderListener listener)
+        {
+            readerListeners.Add(listener);
+        }
+
+        public void NotifyAll(string s)
+        {
+            foreach (IReaderListener readerListener in readerListeners)
+                readerListener.update(s);
         }
     }
 }
--- SerialPortTest/Writer.cs
+++ SerialPortTest/Writer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO.Ports;
 
 namespace SerialPortTest
@@ -5,14 +6,42 @@ namespace SerialPortTest
     interface IWriter
     {
         void write(string s);
+        void Attach(IWriterListener listener);
+    }
+
+    interface IWriterListener
+    {
+        void Update(string s);
     }
 
     class SerialPortWriter : IWriter
     {
-        SerialPort serialPort;
+        List<IWriterListener> writerListeners;
+
         public void write(string s)
         {
+            foreach (IWriterListener writerListener in writerListeners)
+                writerListener.Update(s);
+        }
 
+        public void Attach(IWriterListener listener)
+        {
+            writerListeners.Add(listener);
+        }
+    }
+
+    class SerialPortWriterListener : IWriterListener
+    {
+        SerialPort serialPort;
+
+        public SerialPortWriterListener()
+        {
+            serialPort = new SerialPort();
+        }
+
+        public void Update(string s)
+        {
+            
         }
     }
 }
